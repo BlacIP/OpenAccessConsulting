e9150e09305e213fd9a699b1294989f3b67db4f2
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-15 03:13:37 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-white">
         <Header />
-        <main className="px-10">
+        <main>
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/about" element={<About />} />
