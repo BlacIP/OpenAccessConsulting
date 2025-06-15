da5471fa0c763cc98ff73b0ececc55a7443a41a3
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-15 02:30:49 +0100

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -229,7 +229,7 @@ const Services = () => {
       </section>
 
       {/* Core Services */}
-      <section className="py-20">
+      <section className="py-20 px-28">
         <div className="text-center mb-16">
           <h2 className="text-3xl lg:text-4xl font-bold text-gray-900 mb-4">
             Our Core Services
