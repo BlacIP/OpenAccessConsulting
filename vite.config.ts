b4cc14d12deefc719a0444b7cabb1768c6044784
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-20 02:59:09 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: env.VITE_APP_BASE,
+    base: env.VITE_APP_BASE || '/',
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
