600ef44eca0148ccb6dc1098dd3d2c62b9950996
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-20 03:14:32 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  
+  // Check if it's Netlify environment based on the reserved NETLIFY variable
+  const isNetlify = process.env.NETLIFY === 'true';
+
   return {
-    base: env.VITE_APP_BASE || '/',
+    base: isNetlify ? '/' : env.VITE_APP_BASE,  // Use root for Netlify, base path for GitHub Pages
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
