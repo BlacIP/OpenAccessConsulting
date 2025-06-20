06d23cfe7e9b1f0cea2a109d3280bfeb6b37ccf8
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-20 03:24:38 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
-  // Check if it's Netlify environment based on the reserved NETLIFY variable
+
+  // Check if the environment is Netlify
   const isNetlify = process.env.NETLIFY === 'true';
 
+  // Choose the correct base path depending on the environment
+  const basePath = isNetlify 
+    ? '/' // Use root for Netlify
+    : env.VITE_APP_BASE; // Use the base path defined in .env.production or .env.staging
+
   return {
-    base: isNetlify ? '/' : env.VITE_APP_BASE,  // Use root for Netlify, base path for GitHub Pages
+    base: basePath,
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
