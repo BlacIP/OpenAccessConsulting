cbf9e41b562928e74cfa854d22cf5222cd7a21ed
Boluwatife Adebiyi <122327963+BlacIP@users.noreply.github.com>
2025-06-23 18:13:05 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="bg-white shadow-sm border-b border-gray-200 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-10 lg:px-28">
+      <div className="mx-auto px-4 sm:px-10 lg:px-28">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2 group">
